c87d7ea8d28c1499c737134edfb94db3c19207e1
Wei Lin <wwlin@student.umass.edu>
2013-12-09 22:32:54 -0500

    fname and lname validation

--- public/javascripts/sidemenu_index.js
+++ public/javascripts/sidemenu_index.js
@@ -2,6 +2,7 @@
  * Created by Wei on 11/2/13.
  */
 
+<!-- this will toggle the side menu appears or not if you clik on the sidebar-wrapper -->
 $("#menu-close").click(function(e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
@@ -12,6 +13,7 @@ $("#menu-toggle").click(function(e) {
     $("#sidebar-wrapper").toggleClass("active");
 });
 
+<!-- this will take the id and if you click on it, it will scroll to that section -->
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
